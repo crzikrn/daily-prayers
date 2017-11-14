0e574c25d082c90fd65a03318701a36cf902cb3d
Karam Byun <byunkaram@gmail.com>
2017-11-13 22:15:49 -0500

    Update script.js
    Updated css for background adding fixed

--- script.js
+++ script.js
@@ -17,21 +17,21 @@ $(function(){
     if ( hour >= 5 && hour <= 11) {
         $('#morning').show();
         console.log("it's morning");
-        $('body').css("background","linear-gradient(135deg, cyan, 60%, yellow)");
+        $('body').css("background","linear-gradient(135deg, cyan, 60%, yellow) fixed");
         $('body').css("color","#333");
     }
     // Midday Prayers 11am - 4pm
     else if ( hour >= 11 && hour <= 12) {
         $('#afternoon').show();
         console.log("it's afternoon");
-        $('body').css("background","linear-gradient(135deg, orange, 60%, yellow)");
+        $('body').css("background","linear-gradient(135deg, orange, 60%, yellow) fixed");
         $('body').css("color","#333");
     }
   
     else if( hour >= 11 || hour <= 2) {
         $('#evening').show();
         console.log("it's evening");
-        $('body').css("background","linear-gradient(135deg, navy, 60%, purple)");
+        $('body').css("background","linear-gradient(135deg, navy, 60%, purple) fixed");
         $('body').css("color","#cecece");
     }
 
